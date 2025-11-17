1d86f5dc7d7bdc6a3f99994455fcfdd6a0c3b48d
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 19:59:07 +0400

    Add App.vue

---
+++ client/src/App.vue
@@ -0,0 +1,6 @@
+<template><Filters @change="onFilter"/><Heatmap :apollo="$apollo" :filter="filter"/></template>
+<script>
+import Filters from './components/Filters.vue'
+import Heatmap from './components/Heatmap.vue'
+export default { components:{Filters,Heatmap}, data:()=>({filter:null}), methods:{onFilter(f){this.filter=f}} }
+</script>
