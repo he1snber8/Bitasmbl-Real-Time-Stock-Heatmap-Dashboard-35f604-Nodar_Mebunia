13b115a5430178df048ad1c69e3f9917414c441d
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 19:59:05 +0400

    Add Heatmap.vue

---
+++ client/src/components/Heatmap.vue
@@ -0,0 +1,6 @@
+<template><canvas ref="canvas"></canvas></template>
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
+export default { props:['apollo','filter'], mounted(){ /* init chart and subscribe to updates via this.apollo */ } }
+</script>
