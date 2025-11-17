48bf28f630a1e3d71933e14d744974d9fc834d06
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 19:59:06 +0400

    Add Filters.vue

---
+++ client/src/components/Filters.vue
@@ -0,0 +1,2 @@
+<template><div><select v-model="s" @change="emit()"><option>All</option><option>Tech</option></select></div></template>
+<script>export default { data:()=>({s:'All'}), methods:{emit(){this.$emit('change', this.s)}} }</script>
